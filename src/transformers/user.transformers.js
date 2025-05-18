ff6b34876cbbebca6751d6d760fe2289ace9a566
NGUYEN XUAN TUAN ANH <91230849+xuananh2212@users.noreply.github.com>
2025-05-18 18:29:15 +0700

    update

--- src/transformers/user.transformers.js
+++ src/transformers/user.transformers.js
@@ -1,17 +1,18 @@
-const Transformer = require('../core/Transformer');
+const Transformer = require("../core/Transformer");
 class UserTransformer extends Transformer {
-     response(instance) {
-          return {
-               id: instance.id,
-               fullName: instance.name,
-               email: instance.email,
-               phone: instance.phone,
-               address: instance.address,
-               avatar: instance.avatar,
-               createdAt: instance.created_at,
-               updatedAt: instance.updated_at,
-          };
-     }
+  response(instance) {
+    return {
+      id: instance.id,
+      fullName: instance.name,
+      email: instance.email,
+      phone: instance.phone,
+      address: instance.address,
+      avatar: instance.avatar,
+      role: instance.role,
+      createdAt: instance.created_at,
+      updatedAt: instance.updated_at,
+    };
+  }
 }
 
 module.exports = UserTransformer;
